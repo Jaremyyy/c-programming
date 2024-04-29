e59678a3ea99f17ab41cf85075e919490acc4190
Jaremyyy <114565627+Jaremyyy@users.noreply.github.com>
2024-04-29 19:46:32 +0300

    Add files via upload

---
+++ task3_prog/task3_prog/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public delegate double CalcDelegate(double num1, double num2, char operation);
+
+namespace task3_prog
+{
+    public class CalcProgram
+    {
+        public static double Calc(double num1, double num2, char operation)
+        {
+            switch (operation)
+            {
+                case '+':
+                    return num1 + num2;
+                case '-':
+                    return num1 - num2;
+                case '*':
+                    return num1 * num2;
+                case '/':
+                    if (num2 != 0)
+                        return num1 / num2;
+                    else
+                        return 0;
+                default:
+                    throw new ArgumentException("Wrong operation!");
+            }
+        }
+
+        public CalcDelegate funcCalc = new CalcDelegate(Calc);
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalcProgram calculator = new CalcProgram();
+
+            Console.WriteLine(calculator.funcCalc(25, 8, '+'));
+            Console.WriteLine(calculator.funcCalc(44, 4, '-'));
+            Console.WriteLine(calculator.funcCalc(25, 5, '*'));
+            Console.WriteLine(calculator.funcCalc(5, 10, '/'));
+            Console.WriteLine(calculator.funcCalc(5, 0, '/'));
+        }
+    }
+}
